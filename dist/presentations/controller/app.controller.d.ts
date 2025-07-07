6aa3d13440794a076b22851fdc9360466dff6913
abuzar996 <abuzar.rahim70@hotmail.com>
2025-07-07 13:17:00 +0500

    final bootstrapping

---
+++ dist/presentations/controller/app.controller.d.ts
@@ -0,0 +1,6 @@
+import { AppService } from "../../applications/use-case/app.service";
+export declare class AppController {
+    private readonly appService;
+    constructor(appService: AppService);
+    getHello(): string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./presentations/module/app.module";
-
+import * as bodyParser from "body-parser";
+import Helmet from "helmet";
+import rateLimit from "express-rate-limit";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle("Brick&Mortars.ai")
+    .setDescription("Endpoints")
+    .setVersion("1.0")
+    .addTag("User")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+  app.use(bodyParser.json({ limit: "50mb" }));
+  app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+  app.use(
+    rateLimit({
+      windowMs: 60 * 60 * 1000,
+      max: 1000,
+      message: "Too many requests, please try again after an hour",
+      standardHeaders: true,
+      legacyHeaders: false,
+    })
+  );
+  app.use(Helmet());
+  app.enableCors();
+  app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

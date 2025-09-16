3949488dca127592d16cb2400a14452d9d09b406
abuzar996 <abuzar.rahim70@hotmail.com>
2025-09-16 17:45:41 +0500

    add redis integration

---
+++ dist/presentations/dto/person-counting/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AddEntryDto = void 0;
+const add_entry_dto_1 = require("./add-entry.dto");
+Object.defineProperty(exports, "AddEntryDto", { enumerable: true, get: function () { return add_entry_dto_1.AddEntryDto; } });
+//# sourceMappingURL=index.js.map

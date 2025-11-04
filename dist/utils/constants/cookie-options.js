b3360b6d125636b0c4aa81a46b7aa6b11b59ccd8
abuzar996 <abuzar.rahim70@hotmail.com>
2025-11-04 15:18:50 +0500

    cheange cookies options

--- dist/utils/constants/cookie-options.js
+++ dist/utils/constants/cookie-options.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.cookiesOptions = void 0;
 exports.cookiesOptions = {
     httpOnly: true,
-    sameSite: 'none',
-    secure: true,
+    sameSite: !process.env.STORE ? 'none' : 'lax',
+    secure: !process.env.STORE ? true : false,
     maxAge: 24 * 60 * 60 * 1000,
 };
 //# sourceMappingURL=cookie-options.js.map
--- src/utils/constants/cookie-options.ts
+++ src/utils/constants/cookie-options.ts
@@ -2,7 +2,7 @@ import { CookieOptions } from 'express';
 
 export const cookiesOptions: CookieOptions = {
   httpOnly: true,
-  sameSite: 'none',
-  secure: true,
+  sameSite: !process.env.STORE ? 'none' : 'lax',
+  secure: !process.env.STORE ? true : false,
   maxAge: 24 * 60 * 60 * 1000,
 };
